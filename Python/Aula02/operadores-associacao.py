c623cf6b21564685b1f93074098efa7d4bed97cb
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-14 13:17:50 -0300

    Create operadores-associacao.py

---
+++ Python/Aula02/operadores-associacao.py
@@ -0,0 +1,11 @@
+#Operadores de Associação
+
+curso = 'Curso de Python'
+frutas = ['laranja', 'uva', 'limão']
+saques = ['1500', '100']
+
+print('Python' in curso)
+
+print('uva' in frutas)
+print('maça' in frutas)
+print('limão' not in frutas)
