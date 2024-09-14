0526474fd9591c9ecbfc43270752a2643e51ecfc
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-14 12:26:17 -0300

    Create operadores-atribuicao.py

---
+++ Python/Aula02/operadores-atribuicao.py
@@ -0,0 +1,22 @@
+#Operadores de Atribuição
+
+saldo = 500
+print(saldo)
+
+saldo += 200
+print(saldo)
+
+saldo -= 200
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo *= 20
+print(saldo)
+
+saldo %= 3
+print(saldo)
+
+saldo **= 2
+print(saldo)
