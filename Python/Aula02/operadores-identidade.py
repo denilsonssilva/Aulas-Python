13fe0b0ec34a23083a717c9bd6297ff11e752388
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-14 13:09:41 -0300

    Create operadores-identidade.py

---
+++ Python/Aula02/operadores-identidade.py
@@ -0,0 +1,13 @@
+#Operadores de Identidade
+
+curso = 'Curso de Python'
+nome_curso = curso
+saldo, limite = 200, 200
+
+
+curso is nome_curso
+
+curso is not nome_curso
+
+print(saldo is limite)
+print(saldo is not limite)
