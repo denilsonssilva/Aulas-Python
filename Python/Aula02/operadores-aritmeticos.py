ac8873ea4bd8c1024fec77ab9b5f0587f7919a1a
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-14 10:56:28 -0300

    Update operadores-aritmeticos.py

--- Python/Aula02/operadores-aritmeticos.py
+++ Python/Aula02/operadores-aritmeticos.py
@@ -1,9 +1,19 @@
-#Operadores Aritméticos
-
 produto_1 = 10
+
 produto_2 = 20
 
 print(produto_1 + produto_2)
 print(produto_1 - produto_2)
 print(produto_1 / produto_2)
 print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+
+x = (10 + 5) * 4
+
+y = (10 / 2) * 4
+
+print(x)
+print(y)
