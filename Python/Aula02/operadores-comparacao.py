2b868601534a05c47fd802c32ff675131af4eb25
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-14 11:51:46 -0300

    Create operadores-comparacao.py

---
+++ Python/Aula02/operadores-comparacao.py
@@ -0,0 +1,12 @@
+#Operadores de comparação
+
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
+
