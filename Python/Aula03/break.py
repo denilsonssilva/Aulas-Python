bcd35adb311ae50925243eb8cedbb67a8bbe6086
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-15 15:16:51 -0300

    Create break.py

---
+++ Python/Aula03/break.py
@@ -0,0 +1,20 @@
+#Trabalhando com BREAK
+
+#Apresentando os numeros impares 
+
+while True:
+    num = int(input('Informe um número: '))
+
+    if num == 10:
+        break
+
+    if num % 2 == 0:
+        continue
+
+    
+
+
+    print(num)
+
+
+                
