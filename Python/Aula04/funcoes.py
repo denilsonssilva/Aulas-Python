fa732bd22c6cf770fc09506f0dfba77a12ee8213
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-09-22 09:40:26 -0300

    Create funcoes.py

---
+++ Python/Aula04/funcoes.py
@@ -0,0 +1,15 @@
+def exibir_mensagem():
+    print('Hello World!')
+
+def exibir_mensgagem_2(nome):
+    print(f'Seja bem vindo {nome}!')
+
+def exibir_mensagem_3(nome='Denilson'):
+    print(f'Seja bem vindo {nome}!')
+
+exibir_mensagem()
+exibir_mensgagem_2(nome='Danilo')
+exibir_mensagem_3()
+exibir_mensagem(nome='Charlie')
+
+
